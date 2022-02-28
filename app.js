f2265bde309866de684a4464fcdf829386ebab5a
林佩瑤 <rikkubook@gmail.com>
2022-02-28 21:19:07 +0800

    添加 編輯與刪除功能

--- app.js
+++ app.js
@@ -6,31 +6,51 @@ import fetch from 'node-fetch';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import StudentModal from './models/student.js';
-
+import methodOverride from 'method-override'
 
 //middleware 不論是get或post 都會執行
 app.use(express.static("public")); //css
 app.use(bodyParser.urlencoded({extended:true})); //要添加bodyParser才會轉換POST過來的資料
 app.set("view engin","ejs") //這代表 view engine 我們宣告為 ejs
-
+app.use(methodOverride("_method"))
 
 
 app.get("/", (req, res)=>{
   res.render("index.ejs") //render 去處理
 })
+
+// get
+app.get("/students",async (req, res)=>{ // 畫面
+  try{
+    let data = await StudentModal.find()
+    res.render("students.ejs",{data})
+  }catch{
+    res.send("Error with finding data")
+  }
+})
+app.get("/students/:id", async(req, res)=>{ // 畫面
+  try{
+    console.log(req.params)
+    let {id} =req.params
+    let data = await StudentModal.findOne({id})
+		if(data !== null){ // 前端頁面會沒有data.name
+      res.render("studentPage.ejs",{data})
+    }else{
+      res.send("Cannot find this student")
+    }
+  }catch(e){
+    res.send("Error with finding data")
+    console.log(e)
+  }
+})
+//create
 app.get("/students/insert",(req, res)=>{ // 畫面
   res.render("studentInsert.ejs")
 })
-
-app.get("/students",(req, res)=>{ // 畫面
-  res.render("This is students page")
-})
-
 app.post("/students/insert",(req, res)=>{ //接收
   console.log(req.body)
   let {id, name, age, merit, other} = req.body
   let newStudent = new StudentModal({id, name, age, merit, other})
-
   newStudent.save()
   .then(()=>{
     console.log("newStudent save into DB")
@@ -41,6 +61,53 @@ app.post("/students/insert",(req, res)=>{ //接收
     res.render("reject.ejs")
   })
 
+})
+// update
+app.get("/students/edit/:id", async(req, res)=>{ // 畫面
+  try{
+    console.log(req.params)
+    let {id} =req.params
+    let data = await StudentModal.findOne({id})
+		if(data !== null){ // 前端頁面會沒有data.name
+      res.render("edit.ejs",{data})
+    }else{
+      res.send("Cannot find this student")
+    }
+  }catch(e){
+    res.send("Error with finding data")
+    console.log(e)
+  }
+})
+app.put("/students/edit/:id", async(req, res)=>{ // 畫面
+  let {id, name, age, merit, other} =req.body;
+  console.log(req.body) // 獲得數值帶回去
+  res.send("thanks for send")
+  try {
+    let d = await StudentModal.findOneAndUpdate(
+      { id },
+      { id, name, age, scholarship:{merit, other}},
+      { new: true, runValidators: true,}
+      )
+    res.redirect(`students/${id}`) //重新導向
+  }catch{
+    res.render("reject.ejs")
+  }
+})
+//delete
+app.delete("/students/delete/:id",()=>{
+  let{id} = req.params;
+  StudentModal.deleteOne(id).then(()=>{
+    res.send("Deleted Student")
+  }).catch((e)=>{
+    console.log(e)
+    res.send("Deleted failed")
+  })
+})
+
+
+app.get("/*",async (req, res)=>{ // 畫面
+  res.status(404);
+  res.send("Not allowed")
 })
 // connect to mongodb
 mongoose.connect('mongodb://localhost:27017/studentDB').then(()=>{
