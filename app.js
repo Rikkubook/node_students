fb0f1a5b22906e4f01f81f8bd19050bc1cf75e60
林佩瑤 <rikkubook@gmail.com.tw>
2022-03-01 14:21:15 +0800

    修改為 打POSTMAN

--- app.js
+++ app.js
@@ -6,14 +6,22 @@ import fetch from 'node-fetch';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import StudentModal from './models/student.js';
-import methodOverride from 'method-override'
+import methodOverride from 'method-override';
 
 //middleware 不論是get或post 都會執行
 app.use(express.static("public")); //css
 app.use(bodyParser.urlencoded({extended:true})); //要添加bodyParser才會轉換POST過來的資料
+app.use(methodOverride("_method"));
 app.set("view engin","ejs") //這代表 view engine 我們宣告為 ejs
-app.use(methodOverride("_method"))
 
+// connect to mongodb
+await mongoose.connect('mongodb://localhost:27017/studentDB')
+.then(()=>{
+  console.log("Connected to DB")
+}).catch((err)=>{
+  console.log("Connected Failed")
+  console.log(err)
+})
 
 app.get("/", (req, res)=>{
   res.render("index.ejs") //render 去處理
@@ -23,9 +31,9 @@ app.get("/", (req, res)=>{
 app.get("/students",async (req, res)=>{ // 畫面
   try{
     let data = await StudentModal.find()
-    res.render("students.ejs",{data})
+    res.send(data)　// 打到POSTMAN 確認資料
   }catch{
-    res.send("Error with finding data")
+    res.send({message:"Error with finding data"})
   }
 })
 app.get("/students/:id", async(req, res)=>{ // 畫面
@@ -33,13 +41,17 @@ app.get("/students/:id", async(req, res)=>{ // 畫面
     console.log(req.params)
     let {id} =req.params
     let data = await StudentModal.findOne({id})
+    console.log(data)
 		if(data !== null){ // 前端頁面會沒有data.name
-      res.render("studentPage.ejs",{data})
+      res.send(data)
     }else{
-      res.send("Cannot find this student")
+      res.status(404)
+      res.send({message: "Cannot find this student"})
     }
+    res.send(data)　// 打到POSTMAN 確認資料
   }catch(e){
-    res.send("Error with finding data")
+    res.status(404)
+    res.send({message:"Error with finding data"})
     console.log(e)
   }
 })
@@ -53,12 +65,9 @@ app.post("/students/insert",(req, res)=>{ //接收
   let newStudent = new StudentModal({id, name, age, merit, other})
   newStudent.save()
   .then(()=>{
-    console.log("newStudent save into DB")
-    res.render("accept.ejs")
+    res.send({message: "newStudent save into DB"})
   }).catch((e)=>{
-    console.log("error")
-    console.log(e)
-    res.render("reject.ejs")
+    res.send(e)
   })
 
 })
@@ -81,16 +90,16 @@ app.get("/students/edit/:id", async(req, res)=>{ // 畫面
 app.put("/students/edit/:id", async(req, res)=>{ // 畫面
   let {id, name, age, merit, other} =req.body;
   console.log(req.body) // 獲得數值帶回去
-  res.send("thanks for send")
   try {
     let d = await StudentModal.findOneAndUpdate(
       { id },
       { id, name, age, scholarship:{merit, other}},
-      { new: true, runValidators: true,}
+      { new: true, runValidators: true,} // PUT如果只有部分更新  沒帶參數時跑驗證會錯
       )
-    res.redirect(`students/${id}`) //重新導向
-  }catch{
-    res.render("reject.ejs")
+    res.send({message:"Successfully updated"})
+  }catch(e){
+    res.status(404)
+    res.send(e)
   }
 })
 //delete
@@ -109,48 +118,7 @@ app.get("/*",async (req, res)=>{ // 畫面
   res.status(404);
   res.send("Not allowed")
 })
-// connect to mongodb
-mongoose.connect('mongodb://localhost:27017/studentDB').then(()=>{
-  console.log("Connected to DB")
-}).catch((err)=>{
-  console.log("Connected Failed")
-  console.log(err)
-})
 
-
-// // defined a schema
-// const studentSchema = new mongoose.Schema({
-//   name: String,
-//   age: Number,
-//   major: String,
-//   scholarship:{
-//     merit: Number,
-//     other: Number
-//   }
-// })
-
-// // create a modal for students 會自動轉為 students
-// // model 內第一個字大寫且單數
-// const StudentModal = mongoose.model("Student", studentSchema)
-
-// // Mongo Shell db.collection
-// const Jon = StudentModal({
-//   name: "Jon Benson",
-//   age: 25,
-//   major: "EE",
-//   scholarship:{
-//     merit: 2500,
-//     other: 1300
-//   }
-// })
-
-// // save Jon toDB
-// Jon.save().then(()=>{
-//   console.log("save into DB")
-// }).catch((e)=>{
-//   console.log("error")
-//   console.log(e)
-// })
 
 app.listen(3000, ()=>{
   console.log("Server is running on port 3000")
