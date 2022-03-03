ae50f82064ebf4ba6688479808f8125dd656a8fb
林佩瑤 <rikkubook@gmail.com.tw>
2022-03-03 11:30:07 +0800

    添加 Error Handling

--- app_1.js
+++ app_1.js
@@ -1,17 +1,74 @@
-const express = require("express");
+import express from 'express';
 const app = express();
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
 
 app.set("view engine", "ejs");
 // middlewares
 app.use(express.static("public"));
 
+const monkeySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 5
+  }
+})
+const Monkey = mongoose.model("Monkey",monkeySchema)
+
+app.get("/", async(req, res, next)=>{
+  try{ //在 promise 下 的錯誤要用 try catch + next 處理
+    let newMonkey = new Monkey({name: "CJ"})
+    newMonkey.save()
+    .then(()=>{
+      res.send("data has been saved")
+    }).catch((errMsg)=>{ // 一定要寫 validator error has to be caught by catch
+      res.send(errMsg)
+    })
+  }catch(e){
+    next(e)
+  }
+})
+
+app.get("/monkeyUpdate", async (req, res, next)=>{
+  try{ //在 promise 下 的錯誤要用 try catch + next 處理
+    await Monkey.findOneAndUpdate(
+      {name: "Benson"},
+      {name:"CJ"},
+      {new: true, runValidator: true},
+      (err,doc)=>{
+        if(err){
+          res.send(err);
+        } else {
+          res.send(doc);
+        }
+      }
+    )
+  }catch(e){
+    next(e)
+  }
+})
+
+app.get("/monkey", async (req, res, next)=>{
+  try{ //在 promise 下 的錯誤要用 try catch + next 處理
+    let findMonkey = await Monkey.findOne({name: "Benson k."})
+    res.send(findMonkey)
+  }catch(e){
+    next(e)
+  }
+})
+
+app.get("/*",async (req, res)=>{ // 畫面
+  res.status(404);
+  res.send("Not allowed")
+})
+
+// 放在最下方的 middleware 如果上方 app.get() 有任何錯誤會跑到這
+app.use((err, req, res, next)=>{
+  console.error(err)
+  res.status(500).send("something is broken")
+})
+
 mongoose
-  .connect("mongodb://localhost:27017/test", {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect("mongodb://localhost:27017/test")
   .then(() => {
     console.log("Connected to mongodb.");
   })
